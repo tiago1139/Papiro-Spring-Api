7654ee5f53a52317e8f63cf491fd010e10f6eede
Tiago Pinto <tiagopinto.dev@gmail.com>
2022-09-10 15:07:15 +0100

    Update UserController.java

--- src/main/java/com/tiagopinto/papiroapp/controller/UserController.java
+++ src/main/java/com/tiagopinto/papiroapp/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
-@CrossOrigin("https://tiago1139.github.io/Papiro-Angular")
+@CrossOrigin(origins= "https://tiago1139.github.io/Papiro-Angular")
 public class UserController {
 
     private UserRepository userRepository;
