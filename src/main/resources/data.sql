e2eecec6467a5b25c20bfa2665097891fd8fb842
Tiago Pinto <tiagopinto.dev@gmail.com>
2022-09-10 16:13:08 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO "Book" (id, title, author, cover) VALUES (1, 'Book 1', 'Tiago', 'http://localhost:8080/api/images/1_cover.jpg')
-INSERT INTO "Book" (id, title, author, cover) VALUES (2, 'Book 2', 'Tiago 2', 'http://localhost:8080/api/images/2_cover.jpg')
+INSERT INTO book (id, title, author, cover) VALUES (1, 'Book 1', 'Tiago', 'http://localhost:8080/api/images/1_cover.jpg')
+INSERT INTO book (id, title, author, cover) VALUES (2, 'Book 2', 'Tiago 2', 'http://localhost:8080/api/images/2_cover.jpg')
 
 
-INSERT INTO "Users" (id, username, password, firstname, lastname) VALUES (1, 'tiago', '123', 'Tiago', 'Pinto')
-INSERT INTO "Users" (id, username, password, firstname, lastname) VALUES (2, 'admin', 'admin123', 'Admin', 'Admin')
+INSERT INTO users (id, username, password, firstname, lastname) VALUES (1, 'tiago', '123', 'Tiago', 'Pinto')
+INSERT INTO users (id, username, password, firstname, lastname) VALUES (2, 'admin', 'admin123', 'Admin', 'Admin')
